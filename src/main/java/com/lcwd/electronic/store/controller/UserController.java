74ba9a1db027847da8e9b839ce4a7e371af5ed2f
Abhishek Patel <abhishekpatel1238@gmail.com>
2023-12-07 16:10:09 +0530

    (11.150) @Pattern for regular Expression for validation

--- src/main/java/com/lcwd/electronic/store/controller/UserController.java
+++ src/main/java/com/lcwd/electronic/store/controller/UserController.java
@@ -4,6 +4,7 @@ import com.lcwd.electronic.store.dtos.ApiResponseMessage;
 import com.lcwd.electronic.store.dtos.UserDto;
 import com.lcwd.electronic.store.entities.User;
 import com.lcwd.electronic.store.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,25 +19,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-
     //Create-------------------------------------------------------------------------------------------------------
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto)
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto)
     {
         UserDto userDto1 = userService.createUser(userDto);
         return new ResponseEntity<>(userDto1, HttpStatus.CREATED);
     }
 
-
     //Update------------------------------------------------------------------------------------------------------
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") String userId ,@RequestBody UserDto userDto)
+    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") String userId , @Valid @RequestBody UserDto userDto)
     {
         UserDto userDto2 = userService.updateUser(userDto, userId);
         return new ResponseEntity<>(userDto2, HttpStatus.OK);
     }
-
-
 
     //Delete----------------------------------------------------------------------------------------------------
     @DeleteMapping("/{userId}")
@@ -48,13 +45,11 @@ public class UserController {
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
-
     //Get All--------------------------------------------------------------------------------------------------
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers(){
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
-
 
     //Get Single--------------------------------------------------------------------------------------------
     @GetMapping("/{userId}")
@@ -63,7 +58,6 @@ public class UserController {
         return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
     }
 
-
     //Get by email-------------------------------------------------------------------------------------------
     @GetMapping("/email/{email}")
     public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email)
@@ -71,12 +65,9 @@ public class UserController {
         return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
     }
 
-
     //Search User-------------------------------------------------------------------------------------------
     @GetMapping("/search/{keywords}")
     public ResponseEntity<List<UserDto>> searchUser(@PathVariable String keywords){
         return new ResponseEntity<>(userService.searchUser(keywords), HttpStatus.OK);
     }
-
-
 }
--- src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
+++ src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
@@ -1,6 +1,9 @@
 package com.lcwd.electronic.store.dtos;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -12,11 +15,26 @@ public class UserDto {
 
 
     private String userId;
+
+    @Size(min = 3, max = 15, message = "Invalid Name!!")
     private String name;
+
+    @Email(message = "Invalid User Email!!")
+    @NotBlank(message = "Email is not blank!!")
     private String email;
+
+    @NotBlank(message = "Password is required!!")
     private String password;
+
+    @Size(min = 4, max = 6, message = "Invalid gender!!")
     private String gender;
+
+    @NotBlank(message = "Write something about yourself!!")
     private String about;
+
+     //@Pattern
+     //Custom Validator
+
     private String imageName;
 
 }
